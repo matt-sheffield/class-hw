0663de984067e0f1de28b2af630410271eb023f6
matt-sheffield <matthew.sheffield75@gmail.com>
2022-09-20 14:20:35 -0700

    Cleaned up async_await hw

--- async_await_hw/async.js
+++ async_await_hw/async.js
@@ -1,24 +1,3 @@
-/*
-DONE Write an asynchronous function and call it with async/await
-DONE Write function called getRandomNumber that returns a promise
-DONE In the promise, write a setTimeout() that will resolve the promise with a random number after .5 seconds
-DONE Write an async function that invokes the getRandomNumber function, awaiting the random number, and printing that to the console.
-DONE Invoke your async function
-
-
-DONE Fetch remote data using async/await
-DONE Write an asynchronous function that accepts 1 parameter
-DONE A city name
-DONE When invoked, this function should use geocode.xyz's API retrieve the city data, which uses a URL formatted as follows:
-
-https://geocode.xyz/seattle?json=1
-
-Once retrieved, print to the console the latitude and longitude
-Invoke your async function with a few cities as a test
-*/
-
-//====================================================================
-
 function getRandomNumber() {
     return parseInt(Math.random()*10);
 };
