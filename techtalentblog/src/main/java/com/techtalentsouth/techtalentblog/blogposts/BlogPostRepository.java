0a56118caad8de3c1c4007fa31f7175625845d28
matt-sheffield <matthew.sheffield75@gmail.com>
2022-09-19 20:30:10 -0700

    Added blog project

---
+++ techtalentblog/src/main/java/com/techtalentsouth/techtalentblog/blogposts/BlogPostRepository.java
@@ -0,0 +1,11 @@
+package com.techtalentsouth.techtalentblog.blogposts;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlogPostRepository extends CrudRepository<BlogPost, Long> {
+	public List<BlogPost> findAll();
+}
